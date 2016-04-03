da6d37d866416d5891e81e04b4e60748fe21dde9
ryoff <ryoffes@gmail.com>
2016-04-03 22:29:36 +0900

    add spec

--- spec/murasaki_shikibu_spec.rb
+++ spec/murasaki_shikibu_spec.rb
@@ -1,11 +1,64 @@
 require 'spec_helper'
 
+class User < ActiveRecord::Base
+  attribute :name, MurasakiShikibu::Type.new { |name| name.capitalize }
+  attribute :address, MurasakiShikibu::Type.new { |name| name.tr('0-9a-zA-Z', '０-９ａ-ｚＡ-Ｚ') }
+end
+
 describe MurasakiShikibu do
   it 'has a version number' do
     expect(MurasakiShikibu::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#type_cast_from_user' do
+    let(:user) { User.new(name: 'ryoff', address: '東京1-2-3') }
+
+    describe "name" do
+      subject { user.name }
+
+      it { is_expected.to eq 'Ryoff' }
+    end
+
+    describe "address" do
+      subject { user.address }
+
+      it { is_expected.to eq '東京１-２-３' }
+    end
+  end
+
+  describe '#type_cast_for_database' do
+    let(:user) { User.new(name: 'ryoff', address: '東京1-2-3') }
+
+    before { user.save }
+
+    context 'using where' do
+      describe "name" do
+        subject { User.where(name: 'ryoff').first.try!(:name) }
+
+        it { is_expected.to eq 'Ryoff' }
+      end
+
+      describe "address" do
+        subject { User.where(address: '東京1-2-3').first.try!(:address) }
+
+        it { is_expected.to eq '東京１-２-３' }
+      end
+    end
+
+    context 'using find_by' do
+      describe "name" do
+        subject { User.find_by(name: 'ryoff').try!(:name) }
+
+        it { is_expected.to eq 'Ryoff' }
+      end
+
+      describe "address" do
+        subject { User.find_by(address: '東京1-2-3').try!(:address) }
+
+        it { is_expected.to eq '東京１-２-３' }
+      end
+    end
+
+
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require 'active_record'
+require 'pry'
+require 'pry-byebug'
+require 'bundler/setup'
+Bundler.require
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'murasaki_shikibu'
